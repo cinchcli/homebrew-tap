ace92ef7f56434599e803b5b00daa5b7772f167b
Jinmu Go <jingmuio@gmail.com>
2026-05-07 11:46:52 +0900

    fix(cask): add verified param to url stanza for cinch-desktop

--- Casks/cinch-desktop.rb
+++ Casks/cinch-desktop.rb
@@ -5,7 +5,8 @@ cask "cinch-desktop" do
   sha256 arm:   "eb60c4ddebd37011b33e20a80a1af9093647cbdab85a9def5cb43338856c2d6b",
          intel: "d5f0c2a33770421a2e8da92cced3177e11080258c8b3c0e49e9f71ba7d802879"
 
-  url "https://github.com/cinchcli/desktop/releases/download/desktop-v#{version}/Cinch_#{version}_#{arch}.dmg"
+  url "https://github.com/cinchcli/desktop/releases/download/desktop-v#{version}/Cinch_#{version}_#{arch}.dmg",
+      verified: "github.com/cinchcli/desktop/"
   name "Cinch"
   desc "Remote clipboard desktop app"
   homepage "https://cinchcli.com/"
